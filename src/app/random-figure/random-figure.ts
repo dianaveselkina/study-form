d964c76fc2e7f48778bdc29cd878cf45fff09eb9
Diana Veselkina <vess_di@mail.ru>
2025-07-10 00:06:12 +0300

    add random figure

--- src/app/random-figure/random-figure.ts
+++ src/app/random-figure/random-figure.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+interface ImageItem {
+  src: string;
+  class: string;
+}
 
 @Component({
   selector: 'app-random-figure',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './random-figure.html',
-  styleUrl: './random-figure.css'
+  styleUrl: './random-figure.css',
 })
-export class RandomFigure {
+export class RandomFigure implements OnInit, OnDestroy {
+  images: ImageItem[] = [
+    { src: 'assets/circle1.png', class: 'circle figure-size' },
+    { src: 'assets/circle2.png', class: 'circle figure-size' },
+    { src: 'assets/circle3.png', class: 'circle figure-size' },
+    { src: 'assets/circle4.png', class: 'circle figure-size' },
+    { src: 'assets/square1.png', class: 'square figure-size' },
+    { src: 'assets/square2.png', class: 'square figure-size' },
+    { src: 'assets/square3.png', class: 'square figure-size' },
+    { src: 'assets/square4.png', class: 'square figure-size' },
+    { src: 'assets/figure1.png', class: 'figure figure-size' },
+    { src: 'assets/figure2.png', class: 'figure figure-size' },
+    { src: 'assets/figure3.png', class: 'figure figure-size' },
+    { src: 'assets/figure4.png', class: 'figure figure-size' },
+  ];
 
+  currentImage: ImageItem | null = null;
+  intervalId: any;
+
+  ngOnInit() {
+    this.showRandomImage();
+
+    this.intervalId = setInterval(() => {
+      this.showRandomImage();
+    }, 2000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  showRandomImage() {
+    const randomIndex = Math.floor(Math.random() * this.images.length);
+    this.currentImage = this.images[randomIndex];
+  }
 }
